6bdf0cd49d085da0ea44e27acc83ad772900f617
有限 <whnneeww.adventures@outlook.com>
2025-06-27 21:31:43 +0900

    Update trefscoss.js

--- js/trefscoss.js
+++ js/trefscoss.js
@@ -1,1 +1,38 @@
-const trefscoss=(function(){function test(){return"HI!"} function right(){document.querySelectorAll('.right').forEach(box=>{const ddecw=document.documentElement.clientWidth;const rect=box.getBoundingClientRect();const distance=ddecw-rect.left;box.style.transition='transform 0.6s ease, opacity 0.6s ease';box.style.transform=`translateX(${distance}px)`;box.style.opacity='0' return wait(600)})} function left(){document.querySelectorAll('.left').forEach(box=>{const rect=box.getBoundingClientRect();const distance=-rect.left;box.style.transition='transform 0.6s ease, opacity 0.6s ease';box.style.transform=`translateX(${distance}px)`;box.style.opacity='0' 　return wait(600)})} async function fadeout(){await Promise.all([right(),left()])}} return{test,right,left,fadeout}})()
+const trefscoss = (function() {
+  function test() {
+    return "HI!"
+  }
+
+  function right() {
+    document.querySelectorAll('.right').forEach(box => {
+      const ddecw = document.documentElement.clientWidth;
+      const rect = box.getBoundingClientRect();
+      const distance = ddecw - rect.left;
+      box.style.transition = 'transform 0.6s ease, opacity 0.6s ease';
+      box.style.transform = `translateX(${distance}px)`;
+      box.style.opacity = '0'
+      return wait(600)
+    })
+  }
+
+  function left() {
+    document.querySelectorAll('.left').forEach(box => {
+      const rect = box.getBoundingClientRect();
+      const distance = -rect.left;
+      box.style.transition = 'transform 0.6s ease, opacity 0.6s ease';
+      box.style.transform = `translateX(${distance}px)`;
+      box.style.opacity = '0'
+      return wait(600)
+    })
+  }
+  async function fadeout() {
+    await Promise.all([right(), left()])
+  }
+}
+return {
+  test,
+  right,
+  left,
+  fadeout
+}
+})()
